0b8e85f88ab79b0318c93d3c9f9b65c7fc65c885
nobrega <gabrielnobregasantos2@gmail.com>
2026-03-10 23:08:49 -0300

    feat: add reviews route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home';
 import { DrinkDetailsComponent } from './features/drink-details/drink-details';
 import { WishlistComponent } from './features/wishlist/wishlist';
+import { ReviewsComponent } from './features/reviews/reviews';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'drinks/:id', component: DrinkDetailsComponent },
   { path: 'wishlist', component: WishlistComponent },
+  { path: 'reviews', component: ReviewsComponent },
   { path: '**', redirectTo: '' }
 ];
